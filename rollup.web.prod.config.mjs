587896d3221cbf331872e36a31aa707b2c45489f
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-07-02 16:12:38 +0200

    prod build fix

--- rollup.web.prod.config.mjs
+++ rollup.web.prod.config.mjs
@@ -1,6 +1,6 @@
 import { merge } from "webpack-merge"
 import commonMod from "./rollup.web.common.config.mjs"
-import { terser } from "@rollup/plugin-terser"
+import terser from "@rollup/plugin-terser"
 
 
 export default merge(commonMod, {
